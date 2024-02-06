8ceec8f615a11c33719ce3003b402545a8dcab14
AntonS-bio <spadar.anton@gmail.com>
2024-02-06 11:21:17 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
-import os
-import sys
-import subprocess
 from setuptools import setup
-from setuptools.command.install import install
 
 
 def readme():
@@ -14,21 +10,6 @@ def readme():
 # Get the program version from another file.
 __version__ = '0.0.0'
 #exec(open('kleborate/version.py').read())
-
-
-def check_dir_write_permission(directory):
-    if os.path.isdir(directory) and not os.access(directory, os.W_OK):
-        sys.exit('Error: no write permission for ' + directory + '  ' +
-                 'Perhaps you need to use sudo?')
-
-
-def build_blast_db(data_dir, fasta_filename, seq_type):
-    fasta_path = os.path.join(data_dir, fasta_filename)
-    makeblastdb_cmd = ['makeblastdb', '-dbtype', seq_type, '-in', fasta_path]
-    print('  ' + ' '.join(makeblastdb_cmd))
-    with open(os.devnull, 'w') as devnull:
-        subprocess.check_call(makeblastdb_cmd, stdout=devnull)
-
 
 setup(name='HandyAmpliconTool',
       version=__version__,
@@ -44,8 +25,8 @@ setup(name='HandyAmpliconTool',
       author='Anton Spadar',
       author_email='',
       packages=['HandyAmpliconTool'],
-      install_requires=['biopython', 'ncbi-blast'],
-      entry_points={'console_scripts': ['handyamplicontool = run:main']},
-      include_package_data=True,
-      zip_safe=False,
-      cmdclass={'install': none})
+      install_requires=['biopython>=1.78', 'ncbi-blast>=2.9.0'],
+      scripts=[
+          'scripts/run.py'
+      ]
+)
